046143d632f05107097038c0bbb6f0f7f64b0f34
Rajat Kanti Bhattacharjee <rajatthepagal@gmail.com>
2020-05-24 20:43:41 +0530

    ch

--- image-app/src/app/services/constants.ts
+++ image-app/src/app/services/constants.ts
@@ -5,5 +5,5 @@ import { Injectable } from '@angular/core';
   })
 export class ConstantService {
     public readonly apibase = "http://35.208.156.20:3000"
-    public readonly filserver = "http://35.208.156.20:3100"
+    public readonly filserver = "http://35.208.156.20:3000"
 }
